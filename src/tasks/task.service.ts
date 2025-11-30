4edf964c1dc334a0ba3d4a2405f664f3dac2108e
massalyjr9 <157690258+massalyjr9@users.noreply.github.com>
2025-11-30 18:08:20 +0100

    feat(api): ajouter le DTO de création de tâche avec les règles de validation

---
+++ src/tasks/task.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Task, TaskDocument } from './schemas/task.schema';
+import { CreateTaskDto } from './dto/create_task.dto';
+import { UpdateTaskDto } from './dto/update_task.dto';
+
+@Injectable()
+export class TasksService {
+  constructor(
+    // Injection du modèle Mongoose associé à Task
+    @InjectModel(Task.name) // <= IMPORTANT
+    private readonly taskModel: Model<TaskDocument>,
+  ) {}
+
+  create(createTaskDto: CreateTaskDto) {
+    const created = new this.taskModel(createTaskDto);
+    return created.save();
+  }
+
+  findAll() {
+    return this.taskModel.find().exec();
+  }
+
+  async findOne(id: string) {
+    const task = await this.taskModel.findById(id).exec();
+    if (!task) throw new NotFoundException('Task not found');
+    return task;
+  }
+
+  async update(id: string, updateTaskDto: UpdateTaskDto) {
+    const updated = await this.taskModel
+      .findByIdAndUpdate(id, updateTaskDto, { new: true })
+      .exec();
+    if (!updated) throw new NotFoundException('Task not found');
+    return updated;
+  }
+
+  async remove(id: string) {
+    const res = await this.taskModel.findByIdAndDelete(id).exec();
+    if (!res) throw new NotFoundException('Task not found');
+    return res;
+  }
+}
